48ee35194f72aa06e5c0630e6c5578bd77670873
Renat Sergazinov <mrsergazinov@tamu.edu>
2023-10-08 22:00:51 -0500

    Outsource simulation + correct thresholding

--- fd_control.R
+++ fd_control.R
@@ -1,47 +1,26 @@
-library(tidyverse)
-library(ajive)
-source("gen_data.R")
-file_fd <- "results_diffNoise_fd.RData"
-file_metric <- "results_diffNoise_metric.RData"
-load(file_fd)
-load(file_metric)
+thresh <- function(X, sigma = NA){
+  # thresholding using Gavish and Donoho 2014
+  beta = nrow(X) / ncol(X)
+  if (is.na(sigma)){
+    sigma = median(svd(X)$d)
+    omega = 0.56*beta^3 - 0.95*beta^2 + 1.82*beta + 1.43
+    return (omega * sigma)
 
-# set simulation parameters
-set.seed(235017)
-alpha <- 0.9
-rj <- 4
-ri1 <- 10
-ri2 <- 20
-n <- 100
-p1 <- 100
-p2 <- 150
-sigma1 <- 1
-sigma2 <- 10
-sim_iter <- 100
-signal_strength <- 15
-dj <- rnorm(rj, mean = signal_strength, sd = 2)
-di1 <- rnorm(ri1, mean = signal_strength, sd = 2)
-# rep(0, ri1)
-di2 <- rnorm(ri2, mean = signal_strength, sd = 2)
-# rep(0, ri2)
-snr1 <- (sum(dj ** 2) + sum(di1 ** 2)) / sum(n * p1)
-snr2 <- (sum(dj ** 2) + sum(di2 ** 2)) / sum(n * p2)
-snr <- (snr1 + snr2) / 2
-
-thresh <- function(X1, sigma = 1){
-  # thresholding using Gavish and Donoho 2017
-  return(1 + sqrt(min(ncol(X1), nrow(X1)) / max(ncol(X1), nrow(X1)))) 
+  } else {
+    lambda = sqrt(2*(beta+1)+8*beta/((beta+1) + sqrt(beta^2+14*beta+1)))
+    return (lambda * sigma * sqrt(ncol(X)))
+  }
 }
 
-fdControl <- function(X, numSamples=100, alpha=0.7){
+fdControl <- function(X, sigma=NA, numSamples=100, alpha=0.7){
     # X is n x p matrix
     n <- nrow(X)
     p <- ncol(X)
-    avgP <- matrix(0, nrow=n, ncol=n)
+    avgP <- matrix(0, nrow = n, ncol = n)
     for (i in 1:numSamples){
         # resample cols
         Xsample <- X[, sample(1:p, as.integer(p/2), replace=FALSE)]
-        thresh.X = thresh(Xsample) # thresholding singular valuess
+        thresh.X = thresh(Xsample, sigma=sigma) # thresholding singular valuess
         svd.Xsample = svd(Xsample)
         U <- svd.Xsample$u[, svd.Xsample$d > thresh.X] # decompose and extract left singular vectors
         P <- U %*% t(U) # projection matrix
@@ -51,43 +30,3 @@ fdControl <- function(X, numSamples=100, alpha=0.7){
     svdAvg <- svd(avgP) 
     return (svdAvg$u[, svdAvg$d > alpha]) # select left singular vectors for which singular values are > alpha
 }
-
-# create empty table with colnames "SNR", "Method", "Mean", "SD"
-fds <- rep(0, sim_iter)
-metric <- rep(0, sim_iter)
-for (i in 1:sim_iter){
-  data <- gen_data(n, p1, p2, rj, ri1, ri2, dj, di1, di2, sigma1, sigma2)
-  X1 <- data[["X1"]]
-  X2 <- data[["X2"]]
-  Uj <- data[["joint"]]
-  
-  # apply oracle ajive
-  fdControlOut <- fdControl(X2, alpha = alpha)
-  colProjfdControl <- fdControlOut %*% t(fdControlOut)
-  # compute true col space
-  trueProj <- Uj %*% t(Uj)
-  
-  # compute FD and metric
-  fds[i] <- sum(diag((diag(n)-trueProj) %*% colProjfdControl))
-  metric[i] <- sum((trueProj - colProjfdControl)^2)
-}
-# add a row to results.fd
-df <- data.frame(
-  SNR = snr,  # Creating an empty numeric vector for SNR
-  Method = "fdControl X2",
-  Mean = mean(fds),
-  SD = sd(fds)
-)
-results.fd <- rbind(results.fd, df)
-
-df <- data.frame(
-  SNR = snr,  # Creating an empty numeric vector for SNR
-  Method = "fdControl X2",
-  Mean = mean(metric),
-  SD = sd(metric)
-)
-results.metric <- rbind(results.metric, df)
-
-# save results.fd and results.metric
-save(results.fd, file = file_fd)
-save(results.metric, file = file_metric)
--- fd_control_joint.R
+++ fd_control_joint.R
@@ -1,45 +1,24 @@
-library(tidyverse)
-library(ajive)
-source("gen_data.R")
-file_fd <- "results_diffNoise_fd.RData"
-file_metric <- "results_diffNoise_metric.RData"
-load(file_fd)
-load(file_metric)
+thresh <- function(X, sigma = NA){
+  # thresholding using Gavish and Donoho 2014
+  beta = nrow(X) / ncol(X)
+  if (is.na(sigma)){
+    sigma = median(svd(X)$d)
+    omega = 0.56*beta^3 - 0.95*beta^2 + 1.82*beta + 1.43
+    return (omega * sigma)
 
-# set simulation parameters
-set.seed(235017)
-alpha <- 0.9
-rj <- 4
-ri1 <- 10
-ri2 <- 20
-n <- 100
-p1 <- 100
-p2 <- 150
-sigma1 <- 1
-sigma2 <- 10
-sim_iter <- 100
-signal_strength <- 15
-dj <- rnorm(rj, mean = signal_strength, sd = 2)
-di1 <- rnorm(ri1, mean = signal_strength, sd = 2)
-# rep(0, ri1)
-di2 <- rnorm(ri2, mean = signal_strength, sd = 2)
-# rep(0, ri2) 
-snr1 <- (sum(dj ** 2) + sum(di1 ** 2)) / sum(n * p1)
-snr2 <- (sum(dj ** 2) + sum(di2 ** 2)) / sum(n * p2)
-snr <- (snr1 + snr2) / 2
-
-thresh <- function(X1, sigma = 1){
-  # thresholding using Gavish and Donoho 2017
-  return(1 + sqrt(min(ncol(X1), nrow(X1)) / max(ncol(X1), nrow(X1)))) 
+  } else {
+    lambda = sqrt(2*(beta+1)+8*beta/((beta+1) + sqrt(beta^2+14*beta+1)))
+    return (lambda * sigma * sqrt(ncol(X)))
+  }
 }
 
-jointFdControl <- function(X1, X2, numSamples=100, alpha=0.7){
+jointFdControl <- function(X1, X2, sigma1=NA, sigma2=NA, numSamples=100, alpha=0.7){
   avg.P <- matrix(0, nrow=nrow(X1), ncol=nrow(X1))
-  thresh.X1 <- thresh(X1) # thresholding singular values
-  thresh.X2 <- thresh(X2)
   for (i in 1:numSamples){
     X1.sample <- X1[, sample(1:ncol(X1), as.integer(ncol(X1)/2), replace=FALSE)]
     X2.sample <- X2[, sample(1:ncol(X2), as.integer(ncol(X2)/2), replace=FALSE)]
+    thresh.X1 <- thresh(X1.sample) # thresholding singular values
+    thresh.X2 <- thresh(X2.sample)
     svd.X1.sample <- svd(X1.sample)
     svd.X2.sample <- svd(X2.sample)
     u1.sample <- svd.X1.sample$u[, svd.X1.sample$d > thresh.X1] # thresholding using Gavish and Donoho 2014
@@ -52,46 +31,6 @@ jointFdControl <- function(X1, X2, numSamples=100, alpha=0.7){
   out <- svd.avg$u[, svd.avg$d > alpha]
   return(out)
 }
-
-# create empty table with colnames "SNR", "Method", "Mean", "SD"
-fds <- rep(0, sim_iter)
-metric <- rep(0, sim_iter)
-for (i in 1:sim_iter){
-  data <- gen_data(n, p1, p2, rj, ri1, ri2, dj, di1, di2, sigma1, sigma2)
-  X1 <- data[["X1"]]
-  X2 <- data[["X2"]]
-  Uj <- data[["joint"]]
-
-  # apply oracle ajive
-  jointFdControlOut <- jointFdControl(X1, X2, alpha = alpha)
-  colProjJointFdControl <- jointFdControlOut %*% t(jointFdControlOut)
-  # compute true col space
-  trueProj <- Uj %*% t(Uj)
-
-  # compute FD and metric
-  fds[i] <- sum(diag((diag(n)-trueProj) %*% colProjJointFdControl))
-  metric[i] <- sum((trueProj - colProjJointFdControl)^2)
-}
-# add a row to results.fd
-df <- data.frame(
-  SNR = snr,  # Creating an empty numeric vector for SNR
-  Method = "fdControl joint",
-  Mean = mean(fds),
-  SD = sd(fds)
-)
-results.fd <- rbind(results.fd, df)
-
-df <- data.frame(
-  SNR = snr,  # Creating an empty numeric vector for SNR
-  Method = "fdControl joint",
-  Mean = mean(metric),
-  SD = sd(metric)
-)
-results.metric <- rbind(results.metric, df)
-
-# save results.fd and results.metric
-save(results.fd, file = file_fd)
-save(results.metric, file = file_metric)
 
 # test different alphas
 # results = list()
