2a2bf01f5a0936efdd74557155509cbc7e60156d
Renat Sergazinov <mrsergazinov@tamu.edu>
2023-10-17 15:43:31 -0500

    Modified AJIVE to output indiv variation

--- ajive.R
+++ ajive.R
@@ -5,5 +5,7 @@ ajive_wrapper <- function(X1, X2, args){
   ajive_out <- ajive(list(X1, X2), c(args$rj + args$ri1 + 10, args$rj + args$ri2 + 10),
                     n_wedin_samples = 100,
                     n_rand_dir_samples = 100)
-  return(ajive_out$joint_scores)
+  return(list("joint" = ajive_out$joint_scores, 
+              "indiv1" = ajive_out$block_decomps[[1]][['individual']][['u']], 
+              "indiv2" = ajive_out$block_decomps[[2]][['individual']][['u']]))
 }
--- ajive_oracle.R
+++ ajive_oracle.R
@@ -6,5 +6,7 @@ ajive_oracle_wrapper <- function(X1, X2, args){
                     n_wedin_samples = 100, 
                     n_rand_dir_samples = 100,
                     joint_rank = args$rj)
-  return(ajive_out$joint_scores)
+  return(list("joint" = ajive_out$joint_scores, 
+              "indiv1" = ajive_out$block_decomps[[1]][['individual']][['u']], 
+              "indiv2" = ajive_out$block_decomps[[2]][['individual']][['u']]))
 }
