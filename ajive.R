da08d1a3261eda349eac142a5f36c11d86b47bb9
Renat Sergazinov <mrsergazinov@tamu.edu>
2023-11-02 11:24:43 -0500

    Shorten range of acceptable ranks

--- ajive.R
+++ ajive.R
@@ -4,9 +4,9 @@ library(ajive)
 ajive_wrapper <- function(X1, X2, args){
   r1 <- args$rj + args$ri1
   r2 <- args$rj + args$ri2
-  error1 <- sample(seq(-as.integer(r1 / 4), as.integer(r1 / 4), 1), 1)
-  error2 <- sample(seq(-as.integer(r2 / 4), as.integer(r2 / 4), 1), 1)
-  ajive_out <- ajive(list(X1, X2), c(r1 + erro1, r2 + error2),
+  error1 <- sample(seq(-as.integer(r1 / 10), as.integer(r1 / 10), 1), 1)
+  error2 <- sample(seq(-as.integer(r2 / 10), as.integer(r2 / 10), 1), 1)
+  ajive_out <- ajive(list(X1, X2), c(r1 + error1, r2 + error2),
                     n_wedin_samples = 100,
                     n_rand_dir_samples = 100)
   return(list("joint" = ajive_out$joint_scores,
