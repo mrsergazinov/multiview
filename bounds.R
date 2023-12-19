8b42db2584d7468bfa5c2734647efb158f7aa380
Renat Sergazinov <mrsergazinov@tamu.edu>
2023-12-19 11:19:44 +0600

    Fixed bug: accounted for other diags

--- bounds.R
+++ bounds.R
@@ -3,27 +3,25 @@ bound <-function(X1.d, X2.d, rj, ri1, ri2, beta){
   c2 <- (X2.d^4 - beta) / (X2.d^4 + beta*X2.d^2)
   delta1 <- sqrt(1 - c1)
   delta2 <- sqrt(1 - c2)
-  bound <- c()
+  sqrt.delta1 <- sqrt(1 - sqrt(c1))
+  sqrt.delta2 <- sqrt(1 - sqrt(c2))
+  bound <- 0
+  diags <- list()
   for (i in 1:(rj + ri1)) {
     for (j in 1:(rj + ri2)) {
-      if (i != j) {
-        bound = c(bound, (delta1[i] + delta2[j] + delta1[i] * delta2[j]))
+      if (i == j & i < rj){
+        ksi <- sqrt(2) * (sqrt.delta1[i] + 
+                            sqrt.delta2[i] + 
+                            sqrt(2) * sqrt.delta1[i] * sqrt.delta2[i])
+        bound = bound + 2 * ksi + ksi^2
+      } else{
+        eta = delta1[i] + delta2[j] + delta1[i] * delta2[j]
+        diags[[paste(i - j)]] = c(diags[[paste(i - j)]], eta)
       }
     }
   }
-  bound <- 2 * max(bound)
-  for (i in 1:(rj + ri1)) {
-    for (j in 1:(rj + ri2)) {
-      if (i == j & i > rj) {
-        bound = bound + (delta1[i] + delta2[j] + delta1[i] * delta2[j])
-      }
-    }
-  }
-  delta1 <- sqrt(1 - sqrt(c1))
-  delta2 <- sqrt(1 - sqrt(c2))
-  for (i in 1:rj) {
-    ksi <- sqrt(2) * (delta1[i] + delta2[i] + sqrt(2) * delta1[i] * delta2[i])
-    bound <- bound + 2 * ksi + ksi^2
+  for (name in names(diags)) {
+    bound = bound + max(diags[[name]])
   }
   
   return(bound)
