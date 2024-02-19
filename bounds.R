ff0e2489c6dc0c3586250c8aa0cf04b973da878e
Renat Sergazinov <mrsergazinov@tamu.edu>
2024-02-18 18:28:50 -0600

    Added approx bounds

--- bounds.R
+++ bounds.R
@@ -27,9 +27,9 @@ bound <- function(X1.d, X2.d, rj, ri1, ri2, beta){
   return(bound)
 }
 
-bound.approx <- function(X1.d, X2.d, rj, ri1, ri2, beta){
-  c1.min <- min((X1.d^4 - beta) / (X1.d^4 + beta*X1.d^2))
-  c2.min <- min((X2.d^4 - beta) / (X2.d^4 + beta*X2.d^2))
+bound.approx <- function(min.X1, min.X2, rj, ri1, ri2, beta){
+  c1.min <- (min.X1 ^4 - beta) / (min.X1 ^ 4 + beta * min.X1^2)
+  c2.min <- (min.X2 ^4 - beta) / (min.X2 ^ 4 + beta * min.X2^2)
 
   ksi <- sqrt(2) * (sqrt(1 - sqrt(c1.min)) + 
                       sqrt(1 - sqrt(c2.min)) + 
