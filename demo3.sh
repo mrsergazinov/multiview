d4344d924af4833790f36337b20c881f7bef5183
Renat Sergazinov <mrsergazinov@tamu.edu>
2024-03-29 16:18:10 -0500

    Changed for new SNR

--- demo3.sh
+++ demo3.sh
@@ -3,9 +3,9 @@
 Rscript demo3.R sigma1=1 sigma2=1 sigma3=1 phi_max=0 no_joint=FALSE no_indiv=FALSE
 Rscript demo3.R sigma1=1 sigma2=1 sigma3=1 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
 Rscript demo3.R sigma1=1 sigma2=1 sigma3=1 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
-Rscript demo3.R sigma1=2 sigma2=3 sigma3=2 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo3.R sigma1=2 sigma2=3 sigma3=2 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo3.R sigma1=2 sigma2=3 sigma3=2 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
-Rscript demo3.R sigma1=3 sigma2=4 sigma3=3 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo3.R sigma1=3 sigma2=4 sigma3=3 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo3.R sigma1=3 sigma2=4 sigma3=3 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
+Rscript demo3.R sigma1=5 sigma2=6 sigma3=7 phi_max=0 no_joint=FALSE no_indiv=FALSE
+Rscript demo3.R sigma1=5 sigma2=6 sigma3=7 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
+Rscript demo3.R sigma1=5 sigma2=6 sigma3=7 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
+Rscript demo3.R sigma1=10 sigma2=10 sigma3=10 phi_max=0 no_joint=FALSE no_indiv=FALSE
+Rscript demo3.R sigma1=10 sigma2=10 sigma3=10 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
+Rscript demo3.R sigma1=10 sigma2=10 sigma3=10 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
