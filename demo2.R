9ac24740db7573a8f068e57c6c05ba417ef1c5d2
Renat Sergazinov <mrsergazinov@tamu.edu>
2024-03-29 16:06:02 -0500

    Changed SNR calculation -> easier now

--- demo2.R
+++ demo2.R
@@ -7,17 +7,17 @@ library(pracma)
 library(Ckmeans.1d.dp)
 set.seed(235017)
 
-rj <- 2
+rj <- 4
 ri1 <- 3
 ri2 <- 2
-m <- 20
+m <- 50
 phi_max <- 0.5
-n1 <- 100
-n2 <- 120
+n1 <- 80
+n2 <- 100
 sigma1 = 1
 sigma2 = 1
-signal_strength1 <- 30
-signal_strength2 <- 40
+signal_strength1 <- 10
+signal_strength2 <- 12
 rank_spec <- 'exact'
 no_joint <- FALSE
 no_indiv <- TRUE
@@ -245,15 +245,18 @@ for (model in c("ajive", "jive", "slide", "proposed", "proposed_subsampling1", "
   results[[model]] <- matrix(0, sim_iter, 10)
 }
 for (i in 1:sim_iter) {
-  dj1 <- rnorm(rj, mean = signal_strength1, sd = 3)
-  dj2 <- rnorm(rj, mean = signal_strength2, sd = 3)
-  di1 <- rnorm(ri1, mean = signal_strength1, sd = 3)
-  di2 <- rnorm(ri2, mean = signal_strength2, sd = 3)
+  d1 <- svd(matrix(signal_strength1 * rnorm(m * n1), m, n1))$d[1:(rj+ri1)]
+  d2 <- svd(matrix(signal_strength2 * rnorm(m * n2), m, n2))$d[1:(rj+ri2)]
+  d1 <- sample(d1)
+  d2 <- sample(d2)
+  dj1 <- d1[1:rj]
+  dj2 <- d2[1:rj]
+  di1 <- d1[(rj+1):(rj+ri1)]
+  di2 <- d2[(rj+1):(rj+ri2)]
   # generate data
   U <- svd(matrix(rnorm(m * (n1+n2)), m, n1+n2))$u
   # joint parts
   Uj <- U[, 1:rj]
-  Ujperp <- U[, (rj+1):ncol(U)]
   O <- randortho(rj, type = 'orthonormal') # rotate Uj
   Uj1 <- Uj %*% O
   Uj2 <- Uj
@@ -278,9 +281,11 @@ for (i in 1:sim_iter) {
   Vi2 <- svd(matrix(rnorm(m * n2), m, n2))$v[, 1:ri2]
   # combine
   X1 <- Uj1 %*% diag(dj1) %*% t(Vj1) + Ui1 %*% diag(di1) %*% t(Vi1)
-  Y1 <- X1 + matrix(rnorm(m * n1), m, n1) * sigma1
+  Z1 <- matrix(rnorm(m * n1), m, n1) * sigma1
+  Y1 <- X1 + Z1
   X2 <- Uj2 %*% diag(dj2) %*% t(Vj2) + Ui2 %*% diag(di2) %*% t(Vi2)
-  Y2 <- X2 + matrix(rnorm(m * n2), m, n2) * sigma2
+  Z2 <- matrix(rnorm(m * n2), m, n2) * sigma2
+  Y2 <- X2 + Z2
 
   # compute true
   Pjoint <- Uj1 %*% t(Uj1) # true projection
@@ -353,8 +358,8 @@ results.save[["m"]] <- m
 results.save[["phi_max"]] <- phi_max
 results.save[["n1"]] <- n1
 results.save[["n2"]] <- n2
-results.save[["SNR1"]] <- signal_strength1 / (sigma1 * (sqrt(m) + sqrt(n1)))
-results.save[["SNR2"]] <- signal_strength2 / (sigma2 * (sqrt(m) + sqrt(n2)))
+results.save[["SNR1"]] <- signal_strength1 / sigma1
+results.save[["SNR2"]] <- signal_strength2 / sigma2
 results.save[["sigma1"]] <- sigma1
 results.save[["sigma2"]] <- sigma2
 results.save[["signal_strength1"]] <- signal_strength1
