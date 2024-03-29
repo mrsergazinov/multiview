2bc44b939f765eb894d3c15177d832de5435af79
Renat Sergazinov <mrsergazinov@tamu.edu>
2024-03-29 16:17:44 -0500

    Changed setting for new SNR

--- demo2.sh
+++ demo2.sh
@@ -3,23 +3,23 @@
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0 no_joint=FALSE no_indiv=FALSE
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
 
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0 no_joint=TRUE no_indiv=FALSE
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
 
 Rscript demo2.R sigma1=1 sigma2=1 phi_max=0 no_joint=FALSE no_indiv=TRUE
-Rscript demo2.R sigma1=2 sigma2=3 phi_max=0 no_joint=FALSE no_indiv=TRUE
-Rscript demo2.R sigma1=3 sigma2=4 phi_max=0 no_joint=FALSE no_indiv=TRUE
+Rscript demo2.R sigma1=5 sigma2=6 phi_max=0 no_joint=FALSE no_indiv=TRUE
+Rscript demo2.R sigma1=10 sigma2=10 phi_max=0 no_joint=FALSE no_indiv=TRUE
