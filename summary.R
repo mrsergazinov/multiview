233994da03d13ffa6d1f8a487a5d03da92dedc98
Renat Sergazinov <mrsergazinov@tamu.edu>
2024-04-17 19:29:59 -0400

    Automatic table generation

--- summary.R
+++ summary.R
@@ -1,9 +1,12 @@
+library(knitr)
 # Function to extract desired parameters from a saved file
 extract_params <- function(file_path) {
   load(file_path)
   
   # Extract desired parameters
   params <- list(
+    f1_jive = results.save$results['jive', "f1"],
+    f1_slide = results.save$results['slide', "f1"],
     f1_ajive = results.save$results['ajive', "f1"],
     f1_proposed = results.save$results['proposed', "f1"],
     f1_proposed_subsampling1 = results.save$results['proposed_subsampling1', "f1"],
@@ -19,7 +22,7 @@ extract_params <- function(file_path) {
 }
 
 # Get paths of all saved files starting with "demo2_"
-file_paths <- list.files(path = './results', pattern = "^demo3_.*\\.RData$", full.names = TRUE)
+file_paths <- list.files(path = './results', pattern = "^demo2_.*\\.RData$", full.names = TRUE)
 
 # Extract parameters from each file
 all_params <- lapply(file_paths, extract_params)
@@ -28,7 +31,21 @@ all_params <- lapply(file_paths, extract_params)
 df <- do.call(rbind, lapply(all_params, data.frame, row.names = NULL))
 
 # Organize the data frame
-df <- df[, c("f1_ajive", "f1_proposed", "f1_proposed_subsampling1", "f1_proposed_subsampling2", "phi.max", "SNR1", "SNR2", "no_joint", "no_indiv")]
+df <- df[, c("f1_jive",
+             "f1_slide",
+             "f1_ajive", 
+             "f1_proposed", 
+             "f1_proposed_subsampling1", 
+             "f1_proposed_subsampling2", 
+             "phi.max", 
+             "SNR1", 
+             "SNR2", 
+             "no_joint", 
+             "no_indiv")]
 
 # Print the data frame
-print(df)
+df_short <- df[1:9, c("f1_jive", "f1_slide", "f1_ajive", "f1_proposed", "f1_proposed_subsampling2")]
+df_short[['D-CCA']] <- rep(0, 9)
+df_short[['DIVAS']] <- rep(0, 9)
+df_short <- cbind(expand.grid(c('high SNR', 'medium SNR', 'low SNR'), c('Orthogonal', 'Slightly aligned', 'Aligned')), df_short)
+kable(df_short[, c('Var2', 'Var1', 'f1_jive', 'D-CCA', 'f1_slide', 'f1_ajive', 'DIVAS', 'f1_proposed', 'f1_proposed_subsampling2')], 'latex')
