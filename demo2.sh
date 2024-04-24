e1064909cd36b0d44959b3e3f6ac7eee785d7cff
mrsergazinov <mrsergazinov@tamu.edu>
2024-04-24 10:02:18 -0500

    Added SBATCH commands for SLURM

--- demo2.sh
+++ demo2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#SBATCH --job-name=jive
+#SBATCH --time=2
+#SBATCH --nodes=1
+#SBATCH --mem=10GB
+#SBATCH --partition=long
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --output=out.%j
+module purge
+module load R Python
+
 Rscript demo2.R snr1=8 snr2=8 phi_max=90 no_joint=FALSE no_indiv=FALSE
 Rscript demo2.R snr1=8 snr2=8 phi_max=60 no_joint=FALSE no_indiv=FALSE
 Rscript demo2.R snr1=8 snr2=8 phi_max=30 no_joint=FALSE no_indiv=FALSE
