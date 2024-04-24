fdce88545ca25af9e8993ad0eab8950a8925f2a4
Renat Sergazinov <mrsergazinov@tamu.edu>
2024-04-24 01:37:57 -0400

    Added simulations

--- demo2.sh
+++ demo2.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-Rscript demo2.R snr1=8 snr2=8 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=8 snr2=8 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=8 snr2=8 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0.5 no_joint=FALSE no_indiv=FALSE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0.8 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=8 snr2=8 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=8 snr2=8 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=8 snr2=8 phi_max=30 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=4 snr2=4 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=4 snr2=4 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=4 snr2=4 phi_max=30 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=2 snr2=2 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=2 snr2=2 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo2.R snr1=2 snr2=2 phi_max=30 no_joint=FALSE no_indiv=FALSE
 
-Rscript demo2.R snr1=8 snr2=8 phi_max=0 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=8 snr2=8 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=8 snr2=8 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0.5 no_joint=TRUE no_indiv=FALSE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0.8 no_joint=TRUE no_indiv=FALSE
+# no joint
+Rscript demo2.R snr1=8 snr2=8 phi_max=90 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=8 snr2=8 phi_max=60 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=8 snr2=8 phi_max=30 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=4 snr2=4 phi_max=90 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=4 snr2=4 phi_max=60 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=4 snr2=4 phi_max=30 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=2 snr2=2 phi_max=90 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=2 snr2=2 phi_max=60 no_joint=TRUE no_indiv=FALSE
+Rscript demo2.R snr1=2 snr2=2 phi_max=30 no_joint=TRUE no_indiv=FALSE
 
-Rscript demo2.R snr1=8 snr2=8 phi_max=0 no_joint=FALSE no_indiv=TRUE
-Rscript demo2.R snr1=4 snr2=4 phi_max=0 no_joint=FALSE no_indiv=TRUE
-Rscript demo2.R snr1=2 snr2=2 phi_max=0 no_joint=FALSE no_indiv=TRUE
+# no individuals
+Rscript demo2.R snr1=8 snr2=8 phi_max=90 no_joint=FALSE no_indiv=TRUE
+Rscript demo2.R snr1=4 snr2=4 phi_max=90 no_joint=FALSE no_indiv=TRUE
+Rscript demo2.R snr1=2 snr2=2 phi_max=90 no_joint=FALSE no_indiv=TRUE
+
+# misspecified ranks
+Rscript demo2.R snr1=8 snr2=8 phi_max=90 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=8 snr2=8 phi_max=60 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=8 snr2=8 phi_max=30 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=4 snr2=4 phi_max=90 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=4 snr2=4 phi_max=60 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=4 snr2=4 phi_max=30 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=2 snr2=2 phi_max=90 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=2 snr2=2 phi_max=60 no_joint=FALSE no_indiv=FALSE rank_spec=1
+Rscript demo2.R snr1=2 snr2=2 phi_max=30 no_joint=FALSE no_indiv=FALSE rank_spec=1
+
+Rscript demo2.R snr1=8 snr2=8 phi_max=90 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=8 snr2=8 phi_max=60 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=8 snr2=8 phi_max=30 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=4 snr2=4 phi_max=90 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=4 snr2=4 phi_max=60 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=4 snr2=4 phi_max=30 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=2 snr2=2 phi_max=90 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=2 snr2=2 phi_max=60 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+Rscript demo2.R snr1=2 snr2=2 phi_max=30 no_joint=FALSE no_indiv=FALSE rank_spec=-1
+
