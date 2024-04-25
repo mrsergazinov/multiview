3ed4518cd51eadd537643fee1adeefe1d87a63cc
mrsergazinov <mrsergazinov@tamu.edu>
2024-04-24 21:56:18 -0400

    Reorg summary

--- summary.R
+++ summary.R
@@ -1,25 +1,32 @@
 library(knitr)
 # Function to extract desired parameters from a saved file
+
 extract_params <- function(file_path) {
   load(file_path)
   
-  # Extract desired parameters
-  params <- list(
-    f1_jive = results.save$results['jive', "f1"],
-    f1_slide = results.save$results['slide', "f1"],
-    f1_ajive = results.save$results['ajive', "f1"],
-    f1_dcca = results.save$results['dcca', "f1"],
-    f1_unifac = results.save$results['unifac', "f1"],
-    f1_proposed = results.save$results['proposed', "f1"],
-    f1_proposed_subsampling = results.save$results['proposed_subsampling', "f1"],
-    phi.max = results.save$phi_max,
-    SNR1 = results.save$SNR1,
-    SNR2 = results.save$SNR2,
-    no_joint = results.save$no_joint,
-    no_indiv = results.save$no_indiv
-  )
+  out <- list()
+  models <- names(results.save$results)
+  for (model in models) {
+    table <- results.save$results[[model]]
+    table[['precision.joint']] <- 1 - table$fdr.Pjoint
+    table[['precision.indiv1']] <- 1 - table$fdr.Pindiv1
+    table[['precision.indiv2']] <- 1 - table$fdr.Pindiv2
+    table[['avg.precision']] <- (table$precision.joint + table$precision.indiv1 + table$precision.indiv2) / 3
+    table[['avg.tpr']] <- (table$tpr.Pjoint + table$tpr.Pindiv1 + table$tpr.Pindiv2) / 3
+    table[['f1']] <- 2 * table$avg.precision * table$avg.tpr / (table$avg.precision + table$avg.tpr)
+    out[[paste0(model, '_avgF1')]] <- mean(table$f1)
+    out[[paste0(model, '_stdF1')]] <- sd(table$f1)
+  }
   
-  return(params)
+  # Extract desired parameters
+  out[['phi_max']] <- results.save$phi_max
+  out[['SNR1']] <- results.save$SNR1
+  out[['SNR2']] <- results.save$SNR2
+  out[['no_joint']] <- results.save$no_joint
+  out[['no_indiv']] <- results.save$no_indiv
+  out[['rank_spec']] <- results.save$rank_spec
+  
+  return(out)
 }
 
 # Get paths of all saved files starting with "demo2_"
