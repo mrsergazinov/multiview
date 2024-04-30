5404319c295ca377170e1c716d2efbc6601541ad
mrsergazinov <mrsergazinov@tamu.edu>
2024-04-30 19:03:56 -0400

    Added demo for 3 views

--- demo3.sh
+++ demo3.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=75 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=45 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=2 snr2=2 snr3=4 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=2 snr2=2 snr3=2 phi_max=75 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=2 snr2=2 snr3=2 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=2 snr2=2 snr3=2 phi_max=45 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=75 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=45 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=75 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=60 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=45 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=90 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=75 no_joint=FALSE no_indiv=FALSE
+Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=60 no_joint=FALSE no_indiv=FALSE
 Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=45 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=90 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=75 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=60 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=4 snr2=4 snr3=4 phi_max=45 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=2 snr2=2 snr3=4 phi_max=90 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=2 snr2=2 snr3=2 phi_max=75 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=2 snr2=2 snr3=2 phi_max=60 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=2 snr2=2 snr3=2 phi_max=45 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=90 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=75 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=60 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=1 snr2=1 snr3=1 phi_max=45 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=90 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=75 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=60 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.75 snr2=0.75 snr3=0.75 phi_max=45 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=90 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=75 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=60 no_joint=FALSE no_indiv=FALSE
-# Rscript demo_3_views.R snr1=0.5 snr2=0.5 snr3=0.5 phi_max=45 no_joint=FALSE no_indiv=FALSE
