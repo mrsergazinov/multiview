3be9332cf3f03e9c8474c0d25c5c4c12077cda0a
mrsergazinov <mrsergazinov@tamu.edu>
2024-09-26 17:37:41 -0400

    De-clutter plot

--- demo_product_density.R
+++ demo_product_density.R
@@ -5,20 +5,18 @@ library(ggplot2)
 source('src/bounds.R')
 
 # plot density of product of two random variables for different values of q1 and q2
-q1 <- seq(0.1, 0.5, by = 0.2)
-q2 <- seq(0.1, 0.5, by = 0.2)
+q1 <- seq(0.1, 0.5, by = 0.1)
+q2 <- seq(0.1, 0.5, by = 0.1)
 data <- data.frame(lambda = numeric(0), 
                    f_lambda = numeric(0),
                    q = numeric(0))
 
 for (i in 1:length(q1)) {
-  for (j in 1:length(q2)) {
     lambda <- seq(0.01, 0.99, by = 0.01)
-    values <- f_lambda(lambda, q1[i], q2[j])
+    values <- f_lambda(lambda, q1[i], q2[i])
     data <- rbind(data, data.frame(lambda = lambda, 
                                    f_lambda = values,
-                                   q = paste0("q1 = ", q1[i], ", q2 = ", q2[j])))
-  }
+                                   q = paste0("q1 = ", q1[i], ", q2 = ", q2[i])))
 }
 ggplot(data, aes(x = lambda, y = f_lambda, color = q)) + 
   geom_line() + 
