32720b2f83b81938876cdd0683760d6442d4b2d6
mrsergazinov <mrsergazinov@tamu.edu>
2024-10-07 23:37:19 -0500

    Add plot for sigma + test different angles

--- demo_epsilon_bootstrap.R
+++ demo_epsilon_bootstrap.R
@@ -1,6 +1,7 @@
 my_lib_path <- "./multiview_rlibs"
 .libPaths(my_lib_path)
 
+library(latex2exp)
 library(reticulate)
 library(denoiseR)
 library(MASS)
@@ -8,6 +9,7 @@ library(ggplot2)
 library(pracma)
 library(RMTstat)
 library(Ckmeans.1d.dp)
+library(gridExtra)
 
 library(foreach)
 library(doParallel)
@@ -21,7 +23,6 @@ rj <- 4
 ri1 <- 5
 ri2 <- 4
 m <- 50
-phi_max <- 90
 n1 <- 80
 n2 <- 100
 snr1 <- 10
@@ -35,38 +36,69 @@ no_joint <- FALSE
 no_indiv <- FALSE
 
 snrs <- c(1, 2, 4, 8, 16, 32)
+phi_maxs <- c(90, 60, 30)
 nsim <- 10
-table <- matrix(0, nrow = length(snrs) * nsim, ncol=2)
-table.sigma <- matrix(0, nrow = length(snrs) * nsim, ncol=2)
+table <- matrix(0, nrow = length(snrs) * length(phi_maxs) * nsim, ncol=3)
+table.sigma <- matrix(0, nrow = length(snrs) * length(phi_maxs) * nsim, ncol=3)
 for (j in 1:length(snrs)) {
   snr <- snrs[j]
-  for (i in 1:nsim) {
-    sigma1 <- (signal_strength1 / snr) / (sqrt(m) + sqrt(n1))
-    sigma2 <- (signal_strength2 / snr) / (sqrt(m) + sqrt(n2))
-    data <- generate_data(m, n1, n2,
-                          rj, ri1, ri2, rank_spec,
-                          signal_strength1, signal_strength2,
-                          sigma1, sigma2,
-                          no_joint, no_indiv,
-                          phi_max)
-    sigma1.hat <- est.sigma(data$Y1)
-    sigma2.hat <- est.sigma(data$Y2)
-    out <- proposed_func(data$Y1, data$Y2, data$rank1, data$rank2, return_scores = TRUE, bootstrap_iters = 200)
-    
-    P1.hat <- out$joint %*% t(out$joint) + out$indiv1 %*% t(out$indiv1)
-    P2.hat <- out$indiv2 %*% t(out$indiv2) + out$joint %*% t(out$joint)
-    E1 <- P1.hat - data$P1
-    E2 <- P2.hat - data$P2
-    P1E1P2 <- data$P1 %*% E1 %*% data$P2
-    P1E2P2 <- data$P1 %*% E2 %*% data$P2
-    P1E1E2P2 <- data$P1 %*% E1 %*% E2 %*% data$P2
-    epsilon <- svd(P1E1P2 + P1E2P2 + P1E1E2P2)$d[1]
-    
-    table.sigma[(j-1)*nsim + i,] <- c(snr, mean(c(sigma1 - sigma1.hat, sigma2 - sigma2.hat)))
-    table[(j-1)*nsim + i,] <- c(snr, mean(out$epsilon) - epsilon)
+  for (k in 1:length(phi_maxs)){
+    phi_max <- phi_maxs[k]
+    for (i in 1:nsim) {
+      sigma1 <- (signal_strength1 / snr) / (sqrt(m) + sqrt(n1))
+      sigma2 <- (signal_strength2 / snr) / (sqrt(m) + sqrt(n2))
+      data <- generate_data(m, n1, n2,
+                            rj, ri1, ri2, rank_spec,
+                            signal_strength1, signal_strength2,
+                            sigma1, sigma2,
+                            no_joint, no_indiv,
+                            phi_max / 180 * pi)
+      sigma1.hat <- est.sigma(data$Y1)
+      sigma2.hat <- est.sigma(data$Y2)
+      out <- proposed_func(data$Y1, data$Y2, data$rank1, data$rank2, return_scores = TRUE, bootstrap_iters = 200)
+      
+      P1.hat <- out$joint %*% t(out$joint) + out$indiv1 %*% t(out$indiv1)
+      P2.hat <- out$indiv2 %*% t(out$indiv2) + out$joint %*% t(out$joint)
+      E1 <- P1.hat - data$P1
+      E2 <- P2.hat - data$P2
+      P1E1P2 <- data$P1 %*% E1 %*% data$P2
+      P1E2P2 <- data$P1 %*% E2 %*% data$P2
+      P1E1E2P2 <- data$P1 %*% E1 %*% E2 %*% data$P2
+      epsilon <- svd(P1E1P2 + P1E2P2 + P1E1E2P2)$d[1]
+      
+      table.sigma[(j-1)*nsim*length(phi_maxs) + (k-1)*nsim + i, ] <- c(phi_max, snr, mean(c(sigma1.hat - sigma1, sigma2.hat - sigma2)))
+      table[(j-1)*nsim*length(phi_maxs) + (k-1)*nsim + i, ] <- c(phi_max, snr, mean(out$epsilon) - epsilon)
+    }
   }
 }
-plot(table, xlab = "SNR", ylab = "bootstrap epsilon - epsilon")
+df <- data.frame(table)
+df.sigma <- data.frame(table.sigma)
+colnames(df) <- c("Angle", "SNR", "Bootstrap epsilon minus epsilon")
+colnames(df.sigma) <- c("Angle", "SNR", "Sigma error")
+plts <- list()
+plts.sigma <- list()
+for (phi_max in phi_maxs) {
+  df1 <- df[df$Angle == phi_max, ]
+  plt <- ggplot(data = df1, aes(x = SNR, y = `Bootstrap epsilon minus epsilon`)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    theme_minimal() +
+    xlab("SNR") +
+    ylab(TeX("$\\hat{\\epsilon} - \\epsilon$")) + 
+    ggtitle(paste("Angle = ", phi_max))
+  plts[[length(plts) + 1]] <- plt
+  
+  df2 <- df.sigma[df.sigma$Angle == phi_max, ]
+  plt <- ggplot(data = df2, aes(x = SNR, y = `Sigma error`)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    theme_minimal() +
+    xlab("SNR") +
+    ylab(TeX("$\\hat{s} - s$"))
+  plts.sigma[[length(plts.sigma) + 1]] <- plt
+}
+grid.arrange(grobs = c(plts, plts.sigma), ncol = 3)
+
 
 # load("data/COADdata.rda")
 # # clean response data: delete 0 valued data and create class idx matrix
